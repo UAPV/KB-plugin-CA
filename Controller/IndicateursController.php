76071332ad069b022996701caa3fb1399abb0c23
tavernij <jade.tavernier@univ-avignon.fr>
2018-06-07 14:35:26 +0200

    test exploite identique que les projet

--- Controller/IndicateursController.php
+++ Controller/IndicateursController.php
@@ -914,8 +914,8 @@ class IndicateursController extends BaseController
                         else
                             $donnees['categories'] = $this->getCategorieExploit($donnees);
 
-                        $this->validAllModif($donnees);
-/*
+                        //$this->validAllModif($donnees);
+
                         if (!array_key_exists($donnees['idProject'], $liste) && !array_key_exists($donnees['idProject'], $listeModif)) {
                             if ($donnees['last_cat'] == '' || $donnees['last_cat'] == null)
                                 $donnees['last_cat'] = 'En anomalie';
@@ -996,7 +996,7 @@ class IndicateursController extends BaseController
                                         $cptNbModif++;
                                 }
                             }
-                        }*/
+                        }
 
                     }
 
